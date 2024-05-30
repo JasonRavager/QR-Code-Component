c6dba3f27310e150f84e67f02596df3c7a32eff6
khallyx <ecvergarajr@gmail.com>
2024-05-30 11:43:40 +0800

    Mobile version done. Restructured the file folder.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light_gray': 'hsl(212, 45%, 89%)',
+        'grayish_blue': 'hsl(220, 15%, 55%)',
+        'dark_blue': 'hsl(218, 44%, 22%)',
+      },
+      fontFamily: {
+        'main': ['Outfit', 'sans-serif'],
+      },
+      fontSize: {
+        'normal': '15px',
+      }
+    },
+  },
+  plugins: [],
+}
+
